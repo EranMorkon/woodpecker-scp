2648c4c9a5e5c075c5168c466a16d6db6f99179c
Andreas Mieke <andreas@mieke.at>
2023-12-29 13:28:13 +0100

    fix: Make scp verbose, so progress can be tracked

--- plugin.sh
+++ plugin.sh
@@ -5,4 +5,4 @@ echo "${PLUGIN_SSH_KEY}" > "/root/.ssh/id_rsa"
 chmod 0600 /root/.ssh/id_rsa
 echo "${PLUGIN_SSH_KNOWN_HOSTS}" > "/root/.ssh/known_hosts"
 chmod 0600 /root/.ssh/known_hosts
-scp -B ${PLUGIN_EXTRA_FLAGS} ${PLUGIN_SOURCE} ${PLUGIN_DESTINATION}
+scp -Bv ${PLUGIN_EXTRA_FLAGS} ${PLUGIN_SOURCE} ${PLUGIN_DESTINATION}
