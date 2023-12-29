4e2763d775f9e78d57dc41a8615eb91b13da22f6
Andreas Mieke <andreas@mieke.at>
2023-12-29 13:17:17 +0100

    fix: SSH file permissions

--- plugin.sh
+++ plugin.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mkdir -p /root/.ssh
+chmod 800 /root/.ssh
 echo "${PLUGIN_SSH_KEY}" > "/root/.ssh/id_rsa"
+chmod 600 /root/.ssh/id_rsa
 echo "${PLUGIN_SSH_KNOWN_HOSTS}" > "/root/.ssh/known_hosts"
+chmod 600 /root/.ssh/known_hosts
 scp -B ${PLUGIN_EXTRA_FLAGS} ${PLUGIN_SOURCE} ${PLUGIN_DESTINATION}
