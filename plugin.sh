8a083f992d7afa26ae69527c7454e4e0d1f8eb72
Andreas Mieke <andreas@mieke.at>
2023-12-29 13:20:31 +0100

    fix: Add leading zero

--- plugin.sh
+++ plugin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir -p /root/.ssh
-chmod 800 /root/.ssh
+chmod 0800 /root/.ssh
 echo "${PLUGIN_SSH_KEY}" > "/root/.ssh/id_rsa"
-chmod 600 /root/.ssh/id_rsa
+chmod 0600 /root/.ssh/id_rsa
 echo "${PLUGIN_SSH_KNOWN_HOSTS}" > "/root/.ssh/known_hosts"
-chmod 600 /root/.ssh/known_hosts
+chmod 0600 /root/.ssh/known_hosts
 scp -B ${PLUGIN_EXTRA_FLAGS} ${PLUGIN_SOURCE} ${PLUGIN_DESTINATION}
