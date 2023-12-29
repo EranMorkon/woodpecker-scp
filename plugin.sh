b54f434cb2984d1c811743ff726638d1cd75c234
Andreas Mieke <andreas@mieke.at>
2023-12-29 13:25:32 +0100

    fix: Remove ambigious chmod for directory

--- plugin.sh
+++ plugin.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 mkdir -p /root/.ssh
-chmod 0800 /root/.ssh
 echo "${PLUGIN_SSH_KEY}" > "/root/.ssh/id_rsa"
 chmod 0600 /root/.ssh/id_rsa
 echo "${PLUGIN_SSH_KNOWN_HOSTS}" > "/root/.ssh/known_hosts"
